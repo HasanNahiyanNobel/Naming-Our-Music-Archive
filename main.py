3372fea06f9b8e6a08a19b96dca50e4c29657670
Hasan Nahiyan Nobel <1405037.hn@ugrad.cse.buet.ac.bd>
2021-04-23 14:54:34 +0600

    Tried to catch the exception when the file already exists.

--- main.py
+++ main.py
@@ -2,14 +2,59 @@
 # Author: Hasan Nahiyan Nobel
 # Copyright: Attribution 4.0 International (CC BY 4.0)
 #
+import ntpath
+import os
 
+# Declare final variables
+PATH_OF_SONGS = "G:\গীতাঞ্জলি\উত্তম সঙ্গীত\_উর্ধ্বগামী\Robert Johnson Complete Temp 1"
+START_OF_RENAME_INDEX = 1
+PRINT_INPUT_TO_CONSOLE = False
+
+
+# Read from input file
 input_file = open("input.txt", 'r')
 input_lines = input_file.readlines()
 input_file.close()
 
-start_index = 1
+
+# Save song names to array
+file_names = []
+index = 1
 for line in input_lines:
 	song_name = line.split('\t')[1].replace('\"', '')
-	file_name = str(start_index) + ". " + song_name
-	print(file_name)
-	start_index += 1
+	file_name = str(index) + ". " + song_name
+	file_names.append(file_name)
+	index += 1
+
+
+# Print song names to console if needed
+if PRINT_INPUT_TO_CONSOLE:
+	for file_name in file_names:
+		print(file_name)
+
+
+# Path of songs to be renamed
+path = PATH_OF_SONGS
+
+
+# Rename the files
+index = START_OF_RENAME_INDEX
+for file_path in os.scandir(path):
+	file_name = ntpath.basename(file_path)
+
+	directory = ntpath.dirname(file_path)
+	file_extension = file_name.rsplit('.',1)[1] # Splits to the last occurrence of '.' and takes the second element (which is the file extension)
+
+	new_file_name = file_names[index-1]
+
+	# Create the new file path
+	new_file_path = directory + "\\" + new_file_name + "." + file_extension
+
+	# Rename that path
+	try:
+		os.rename(file_path.path, new_file_path)
+	except FileExistsError:
+		print("The following file already exists: ")
+		print(new_file_path + "\n")
+
+	index += 1
