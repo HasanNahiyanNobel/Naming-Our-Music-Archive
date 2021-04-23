72a94728c5df4459cbdae24d9f137e8be628512a
Hasan Nahiyan Nobel <1405037.hn@ugrad.cse.buet.ac.bd>
2021-04-23 20:21:52 +0600

    Logs are shown on console as metadata is updated.

--- EditMetadata.py
+++ EditMetadata.py
@@ -6,33 +6,53 @@ import os
 
 from mutagen.easyid3 import EasyID3
 
+# Print all the possible keys
+# print(EasyID3.valid_keys.keys())
+
+
 # Directory of the songs
-path_of_songs = "G:\গীতাঞ্জলি\উত্তম সঙ্গীত\_উর্ধ্বগামী\Robert Johnson Complete Temp 1"
+path_of_songs = "G:\গীতাঞ্জলি\উত্তম সঙ্গীত\_উর্ধ্বগামী\Robert Johnson"
+
 
 # Data to be added
 title = ""
-album = ""
-artist = ""
-albumartist = ""
-genre = ""
-date = ""
+album = "The Complete Recordings"
+artist = "Robert Johnson"
+albumartist = "Robert Johnson"
+genre = "Delta Blues"
+date = "1990"
+originaldate = "1936-1937"
+
 
 # Scan the directory and edit metadata
 tracknumber = 1
 for file_path in os.scandir(path_of_songs):
+	# Load the file
 	audio = EasyID3(file_path)
 
+	# Show it in console
+	print("Processing: " + audio.filename)
+	print("Old data  : " + str(audio))
+
+	# Clear existing metadata
 	for key in audio.keys():
 		audio[key] = ""
 
+	# Add new data
 	audio["title"] = title
 	audio["album"] = album
 	audio["artist"] = artist
 	audio["albumartist"] = albumartist
 	audio["genre"] = genre
 	audio["date"] = date
+	audio["originaldate"] = originaldate
 	audio["tracknumber"] = str(tracknumber)
 
+	# Save the file
 	audio.save()
 
+	# Show in console
+	print("New data  : " + str(audio) + '\n')
+
+	# Increase track number
 	tracknumber += 1
