757236dcf5b4ab697b2a55694f790b0eb788ce38
Hasan Nahiyan Nobel <1405037.hn@ugrad.cse.buet.ac.bd>
2021-04-23 19:26:23 +0600

    Now it clears all the metadata which are editable keys of EasyID3.

--- EditMetadata.py
+++ EditMetadata.py
@@ -8,8 +8,20 @@ from mutagen.easyid3 import EasyID3
 
 PATH_OF_SONGS = "G:\গীতাঞ্জলি\উত্তম সঙ্গীত\_উর্ধ্বগামী\Robert Johnson Complete Temp 1"
 
-ARTIST = "Robert Johnson"
+album = ""
+title = ""
+artist = ""
+albumartist = ""
+tracknumber = ""
+genre = ""
+date = ""
+
+# print(EasyID3.valid_keys.keys())
 
 for file_path in os.scandir(PATH_OF_SONGS):
 	audio = EasyID3(file_path)
-	print(audio)
+
+	for key in audio.keys():
+		audio[key] = ""
+
+	audio.save()
