604157ec0066855658da62c61c8562655453cdfa
Hasan Nahiyan Nobel <1405037.hn@ugrad.cse.buet.ac.bd>
2021-04-23 13:42:29 +0600

    Added author and copyright header.

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+#
+# Author: Hasan Nahiyan Nobel
+# Copyright: Attribution 4.0 International (CC BY 4.0)
+#
+
 input_file = open("input.txt", 'r')
 input_lines = input_file.readlines()
 input_file.close()
