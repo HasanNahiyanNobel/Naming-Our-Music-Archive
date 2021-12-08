ab5c19c794d93f82f85a3abb2e96fde0e4e5af48
Hasan Nahiyan Nobel <nobel.hasan.nahiyan@gmail.com>
2021-12-08 22:22:28 +0600

    Minor changes in starter.

--- src/starter.py
+++ src/starter.py
@@ -1,8 +1,11 @@
 from os import system
 from utilities import format_files
 
+# Format all the files using yapf
 format_files()
 
 # Rename files
 system('py rename_files.py')
+
+# And edit metadata!
 system('py edit_metadata.py')
