645e8b434dd29c838786d6f8b083e2d596039579
Hasan Nahiyan Nobel <nobel.hasan.nahiyan@gmail.com>
2021-12-08 21:35:14 +0600

    Using single quote convention for strings, instead of double quote.

--- src/edit_metadata.py
+++ src/edit_metadata.py
@@ -3,23 +3,22 @@ Author: Hasan Nahiyan Nobel
 Copyright: Attribution 4.0 International (CC BY 4.0)
 """
 import os
-
+from utilities import PATH_OF_SONGS
 from mutagen.easyid3 import EasyID3
 
 # Print all the possible keys
 # print(EasyID3.valid_keys.keys())
 
-# Directory of the songs
-path_of_songs = "G:\গীতাঞ্জলি\উত্তম সঙ্গীত\_উর্ধ্বগামী\Robert Johnson"
+path_of_songs = PATH_OF_SONGS
 
 # Data to be added
-title = ""
-album = "The Complete Recordings"
-artist = "Robert Johnson"
-albumartist = "Robert Johnson"
-genre = "Delta Blues"
-date = "1990"
-originaldate = "1936-1937"
+title = ''
+album = 'The Velvet Underground'
+artist = 'The Velvet Underground'
+albumartist = 'The Velvet Underground'
+genre = 'Folk rock; Rock; Pop'
+date = '1969'
+originaldate = '1969'
 
 # Scan the directory and edit metadata
 tracknumber = 1
@@ -28,28 +27,28 @@ for file_path in os.scandir(path_of_songs):
     audio = EasyID3(file_path)
 
     # Show it in console
-    print("Processing: " + audio.filename)
-    print("Old data  : " + str(audio))
+    print('Processing: ' + audio.filename)
+    print('Old data  : ' + str(audio))
 
     # Clear existing metadata
     for key in audio.keys():
-        audio[key] = ""
+        audio[key] = ''
 
     # Add new data
-    audio["title"] = title
-    audio["album"] = album
-    audio["artist"] = artist
-    audio["albumartist"] = albumartist
-    audio["genre"] = genre
-    audio["date"] = date
-    audio["originaldate"] = originaldate
-    audio["tracknumber"] = str(tracknumber)
+    audio['title'] = title
+    audio['album'] = album
+    audio['artist'] = artist
+    audio['albumartist'] = albumartist
+    audio['genre'] = genre
+    audio['date'] = date
+    audio['originaldate'] = originaldate
+    audio['tracknumber'] = str(tracknumber)
 
     # Save the file
     audio.save()
 
     # Show in console
-    print("New data  : " + str(audio) + '\n')
+    print('New data  : ' + str(audio) + '\n')
 
     # Increase track number
     tracknumber += 1
--- src/rename_files.py
+++ src/rename_files.py
@@ -4,16 +4,16 @@ Copyright: Attribution 4.0 International (CC BY 4.0)
 """
 import ntpath
 import os
+from utilities import PATH_OF_SONGS
 
 # Declare final variables
-PATH_OF_SONGS = ""
 START_OF_RENAME_INDEX = 1
 PRINT_INPUT_TO_CONSOLE = False
 SPLIT_CHAR = '\"'
 COL_INDEX_IN_INPUT = 1
 
 # Read from input file
-input_file = open("../input/input.txt", 'r')
+input_file = open('../io/input.txt', 'r')
 input_lines = input_file.readlines()
 input_file.close()
 
@@ -22,7 +22,7 @@ file_names = []
 index = 1
 for line in input_lines:
     song_name = line.split(SPLIT_CHAR)[COL_INDEX_IN_INPUT].replace('\"', '')
-    file_name = str(index).zfill(2) + ". " + song_name
+    file_name = str(index).zfill(2) + '. ' + song_name
     file_names.append(file_name)
     index += 1
 
@@ -47,14 +47,14 @@ for file_path in os.scandir(path):
     new_file_name = file_names[index - 1]
 
     # Create the new file path
-    new_file_path = directory + "\\" + new_file_name + "." + file_extension
+    new_file_path = directory + '\\' + new_file_name + '.' + file_extension
 
     # Rename that path
-    print("Renaming: " + file_path.path)
+    print('Renaming: ' + file_path.path)
     try:
         os.rename(file_path.path, new_file_path)
-        print("New path: " + new_file_path + "\n")
+        print('New path: ' + new_file_path + '\n')
     except OSError:
-        print("Some terrible error occurred.\n")
+        print('Some terrible error occurred.\n')
 
     index += 1
