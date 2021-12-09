e264c29696ce7a5c9aa54fc7943cc9a44a6405d9
Hasan Nahiyan Nobel <nobel.hasan.nahiyan@gmail.com>
2021-12-09 15:19:05 +0600

    Starter now creates a new folder in output, with the name of album.

--- src/starter.py
+++ src/starter.py
@@ -3,8 +3,13 @@ Author: Hasan Nahiyan Nobel
 Copyright: Attribution 4.0 International (CC BY 4.0)
 """
 
-from os import system
-from utilities import format_files
+from os import system, path, mkdir
+from utilities import (
+    OUTPUT_DIR,
+    album,
+    originaldate,
+    format_files,
+)
 
 # Format all the files using yapf
 format_files()
@@ -14,3 +19,8 @@ system('py rename_files.py')
 
 # And edit metadata!
 system('py edit_metadata.py')
+
+# Create a new folder in output directory
+folder_name = album + ' (' + originaldate + ')'
+output_path = path.join(OUTPUT_DIR, folder_name)
+mkdir(output_path)
