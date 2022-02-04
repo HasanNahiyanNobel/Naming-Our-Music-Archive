9676171cfc4d71136637214a03722b747ed21ff0
Hasan Nahiyan Nobel <hasan.nahiyan.nobel@gmail.com>
2022-02-04 22:56:28 +0600

    Created a script to clear all ID3 data of files in a directory.

---
+++ src/clear_metadata.py
@@ -0,0 +1,17 @@
+import os
+from os import scandir
+from mutagen.id3 import ID3, ID3NoHeaderError
+
+current_dir = os.getcwd()
+print('Removing metadata from: ' + current_dir + '\n')
+
+for file_path in scandir(current_dir):
+    print('Processing: ' + os.path.basename(file_path))
+    try:
+        file = ID3(file_path)
+        print('ID3 found. Deleting all the metadata.')
+        # print(file.pprint())  # Print the metadata
+        file.delete()
+    except ID3NoHeaderError:
+        print('No ID3 found.')
+    print()
